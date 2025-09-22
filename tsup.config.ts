b50bcbbdd62f061e2cd25e8969d94865bbfe27ba
fedaykin-adel <gabriel_adelar@hotmail.com>
2025-09-21 22:40:51 -0300

    blaa

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tsup";
 export default defineConfig({
-  entry: { index: "src/index.ts", provider: "src/provider.tsx" },
-  dts: { entry: ["src/index.ts", "src/provider.tsx"] },
-  format: ["esm","cjs"],
-  target: "es2018",
+  entry: ["src/index.ts", "src/provider.tsx"],
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
   sourcemap: true,
-  clean: true
+  target: "es2018",
 });
